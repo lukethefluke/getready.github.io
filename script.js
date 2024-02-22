49cc6b6bdc03a4bf000bafc40a50bda06d12b536
lukethefluke <37602155+lukethefluke@users.noreply.github.com>
2024-02-22 16:03:13 +1100

    Improved script.js

--- script.js
+++ script.js
@@ -1,92 +1,48 @@
+//Create map instance from URL parameters
+let parameters = new Map();
+
+//URL parameters captured on window load
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
-let destination = urlParams.get('destination');
-let hourLeaving = urlParams.get('hour')*1;
-let minuteLeaving = urlParams.get('minute')*1;
-let listParams = urlParams.get('list');
 
+//set key value pairs in parameters mapping from URL
+parameters.set("destination", urlParams.get('destination'));
+parameters.set("hour", urlParams.get('hour'));
+parameters.set("minute", urlParams.get('minute'));
+parameters.set("list", urlParams.get('list'));
 
-if(destination == "" || destination == null) {
-  destination = 'School';
-}
-if(hourLeaving == "" || hourLeaving == null) {
-  hourLeaving = 10;
-}
-if(minuteLeaving == "" || minuteLeaving == null) {
-  minuteLeaving = 30;
-}
-
-if(listParams == "" || listParams == null) {
-  listParams = "Breakfast,Teeth,Uniform,Lunch & Snacks,Hat,Library Books,Shoes,Sunscreen";
-}
-
-//set time picker from default or url parameter
-
-let hourString = ""
-let minuteString = ""
-
-
-if (hourLeaving < 10) {
-  hourString = "0" + String(hourLeaving)
-} else {
-  hourString = String(hourLeaving)
-}
-if (minuteLeaving < 10) {
-  minuteString = "0" + String(minuteLeaving)
-} else {
-  minuteString = String(minuteLeaving)
+//If parameters values are non-existent, then add defaults.
+function parameterDefaults(value,key) {
+  if (value == "" || value == null) {
+    switch(key) {     
+      case "destination":
+        parameters.set("destination","school");
+        break;
+      case "hour":
+        parameters.set("hour","09");
+        break;
+      case "minute":
+        parameters.set("minute","10");
+        break;
+      case "list":
+        parameters.set("list","Breakfast,Teeth,Uniform,Lunch & Snacks,Hat,Library Books,Shoes,Sunscreen");
+        break;
+    }
+  } 
 }
 
-let timeString = hourString + ":" + minuteString;
+//run parameterDefaults to set URL parameters as default. The page will effectively reload the first time it is opened
+parameters.forEach(parameterDefaults);
 
-document.getElementById("departure-time").value = timeString;
-//console.log(document.getElementById("departure-time").value);
+//Display destination parameter on load
+const displayDest = document.querySelector(".dest-main");
+const overlayDest = document.querySelector("#destination");
+displayDest.innerHTML = parameters.get("destination");
 
-
-//set url parameter or default departure time
-departureTime()
-
-
-//display departure time
-function departureTime() {
-  const now = new Date();
-  const datetime = now.toLocaleString();
-  const hourNow = now.getHours();
-  const minuteNow = now.getMinutes();
-
-  let remText = "Signalling Failure";
-
-  const minutesLeaving = hourLeaving*60 + minuteLeaving;
-  const minutesNow = hourNow*60 + minuteNow;
-
-  let minutesDifference = minutesLeaving - minutesNow;
-
-  if (minutesDifference < 0) {
-    minutesDifference = minutesDifference + 24*60;
-  }
-
-  if (minutesDifference > 23*60) {
-    const minuteDifference = 24*60 - minutesDifference;
-    remText = "+" + minuteDifference + "m"
-    updateURL('hour',String(hourLeaving))
-    updateURL('minute',String(minuteLeaving))
-  } else if (minutesDifference == 0 || minutesDifference == 24*60) {
-    remText = "Leaving Now"
-  } else {
-    const hourDifference = Math.floor(minutesDifference/60)
-
-    const minuteDifference = minutesDifference % 60;
- 
-    remText = hourDifference + "hr " + minuteDifference + "m"
-  }
-  document.getElementById("departs-rem").innerHTML = remText;
-}
-
-//display current time
+//Update and display current time
 function updateTime() {
-  const now = new Date();
-  const datetime = now.toLocaleString();
-  const hourNow = now.getHours();
+  let now = new Date();
+  let hourNow = now.getHours();
   if (hourNow > 12) {
     twelveHours = hourNow - 12;
   } else {
@@ -101,23 +57,119 @@ function updateTime() {
     minuteNow.toString().padStart(2, "0") +
     ":" +
     secondNow.toString().padStart(2, "0");
+
+    getRem(parameters.get("hour"),parameters.get("minute"));
+    displayRem(remainingTime.get("totalMinutesDifference"));
+    departureDisplay();
 }
 
+//Create map instance for remaining time, Delay may be "Scheduled","Now" or "Delayed"
+let remainingTime = new Map();
+
+//getRem will return time difference in minutes, as well as the level of delay
+function getRem(hourLeaving,minuteLeaving) {
+  const now = new Date();
+  const hourNow = now.getHours();
+  const minuteNow = now.getMinutes();
+
+  const totalTimeLeaving = hourLeaving*60 + minuteLeaving*1;
+  const totalTimeNow = hourNow*60 + minuteNow*1;
+  totalMinutesDifference = totalTimeLeaving - totalTimeNow;
+
+  if (totalMinutesDifference > 0) {
+    remainingTime.set("Delay","Departing");
+  } else if (totalMinutesDifference == 0) {
+    remainingTime.set("Delay","DepartingNow");
+  } else if (totalMinutesDifference > -60) {
+    remainingTime.set("Delay","Delayed");
+  } else {
+    remainingTime.set("Delay","Departing");
+  totalMinutesDifference = String(totalMinutesDifference*1 + 24*60);
+  }
+
+  remainingTime.set("totalMinutesDifference", Math.abs(totalMinutesDifference));
+  //console.log(remainingTime)  
+}
+
+
+
+//departureInfo contains leaving information that will be modified and displayed by function departureDisplay
+let departureInfo = new Map();
+
+//displayRem will return a string of the remaining time information (new Departure Time)
+function displayRem(remainingMinutes) {
+
+  const now = new Date();
+  const hourNow = now.getHours();
+  const minuteNow = now.getMinutes();
+
+
+  if (remainingTime.get("Delay") == "DepartingNow") {
+    departureInfo.set("Guide","Departing");
+    departureInfo.set("departureHours","Now");
+    departureInfo.set("departureMinutes","");
+  } else if (remainingTime.get("Delay") == "Departing") {
+    departureInfo.set("Guide","Departing");
+    departureInfo.set("departureHours",String(Math.round(remainingMinutes/60)));
+    departureInfo.set("departureMinutes",String(Math.round(remainingMinutes%60)));
+  } else if (remainingTime.get("Delay") == "Delay") {
+    if (remainingTime > 59 ) {
+      remainingTime = 24*60 - remainingTime;
+      departureInfo.set("Guide","Departing");
+      departureInfo.set("departureHours",toString(Math.round(remainingMinutes/60)));
+      departureInfo.set("departureMinutes",toString(Math.round(remainingMinutes%60)));
+    } else {
+      departureInfo.set("Guide","Delayed");
+      departureInfo.set("departureHours",toString(Math.round(remainingMinutes/60)));
+      departureInfo.set("departureMinutes",toString(Math.round(remainingMinutes%60)));
+    }
+  }
+}
+
+//get value of two departure info elements, and populate from info in displayRem
+function departureDisplay() {
+  let departsText = document.getElementById("departure-text");
+  let departsRem = document.getElementById("departure-rem");
+  departsText.innerHTML = departureInfo.get("Guide");
+
+  let departureHours = departureInfo.get("departureHours");
+  let departureMinutes = departureInfo.get("departureMinutes");
+
+  let textRem = "";
+  console.log(departureInfo.get("departureHours"));
+
+  //Display departsRem propery
+  if (!isNaN(departureInfo.get("departureHours")*1)) {
+    
+    if (departureInfo.get("Guide") =="Departing") {
+      if (departureHours == 0) {
+        textRem = departureMinutes + "m";
+      } else if (departureMinutes == 0) {
+        textRem = departureHours + "hr";
+      } else {
+        textRem = departureHours + "hr " + departureMinutes + "m";
+      }
+    } else if (departureInfo.get("Guide") =="Delayed") {
+      textRem = "+" + departureMinutes + "m";
+    }
+  }
+  //console.log(departureInfo.get("Guide"));
+
+  departsRem.innerHTML = textRem;
+}
+
+//Run updateTime and departureTime every second
+
 setInterval(updateTime, 1000); // Run updateTime() every second
-setInterval(departureTime, 1000); // Run updateTime() every second
+// // Get Remaining time info
+//setInterval(displayRem(remainingTime.get("totalMinutesDifference")), 1000); // Get RemDisplay info
+//setInterval(, 1000); // Display Rem info
 
 
 // define the destination text and allow editing
 const overlay = document.querySelector('.overlay');
 
-//Destination overlay
-
-const displayDest = document.querySelector(".dest-main");
-const overlayDest = document.querySelector("#destination");
-
-displayDest.innerHTML = destination
-overlayDest.value = destination
-
+//Clicking on destination will load the Destination overlay
 displayDest.onclick = function() {
   const element = document.getElementById("overlay");
   element.classList.add("show");
@@ -125,41 +177,13 @@ displayDest.onclick = function() {
   destination.classList.add("show");
 }
 
-
-
-//overlay hide
-overlay.onclick = function() {
-  destination = overlayDest.value;
-  displayDest.innerHTML = destination;
-  const element = document.getElementById("overlay");
-  element.classList.remove("show");
-  const service = document.getElementById("destoverlay");
-  service.classList.remove("show");
-  const departure = document.getElementById("departureoverlay");
-  departure.classList.remove("show");
-  const tasklist = document.querySelector(".listoverlay");
-  tasklist.classList.remove("show"); 
-
-
-  let parameters = {
-    hour: hourLeaving,
-    minute: minuteLeaving,
-    destination: destination,
-    list: listParams
-  } 
-
-
-
-
+//overlay hide will update URL and the page will reload with the new parameters
+overlay.onclick = function() { 
   updateURL(parameters);
-  
-  //updateURL('destination',destination)
-  
 }
 
 //Departure time overlay
-const departure = document.querySelector("#departs-rem");
-
+const departure = document.querySelector("#departure-rem");
 departure.onclick = function() {
   const element = document.getElementById("overlay");
   element.classList.add("show");
@@ -167,20 +191,12 @@ departure.onclick = function() {
   overlaydeparture.classList.add("show");
 }
 
-function timePassed(time) {    
-  let[hours, mins] = time.split(":");
-  hourLeaving = hours*1;
-  minuteLeaving = mins*1;
-  departureTime();
-}
-
 function sendList(stringList) {
   listParams = stringList
-  console.log(listParams)
 }
 
 
-//Create default list
+//Create list from parameters
 
 function createList() {
   let toDo = listParams.split(",")
@@ -192,16 +208,12 @@ function createList() {
     li.innerText = toDo[i];
     toDoList.appendChild(li);
   }
-
   overlaytodolist.innerHTML = listParams
 }
 
-createList();
 
-
-//Overlay for list
+//Overlay for list appears on click
 const tasklist = document.querySelector(".task-list");
-
 tasklist.onclick = function() {
   const element = document.getElementById("overlay");
   element.classList.add("show");
@@ -209,12 +221,7 @@ tasklist.onclick = function() {
   items.classList.add("show");
 }
 
-//accept stringlist, separate based on comma and add to toDo list, 
-//then display on screen and overlay innerHTML
-function listPassed(stringList) {
-  listParams = stringList;
-}
-
+//update the URL. THis will cause the page to reload
 function updateURL(parameters) {
   const urlString = window.location;
   const url = new URL(urlString);
@@ -224,12 +231,5 @@ function updateURL(parameters) {
   Object.keys(newParams).forEach(key => {
     url.searchParams.set(key, newParams[key]);
   });
-
   window.location = url
-
-  console.log(`${url}`)
 }
-
-//todo
-//default departure time based on hourLeaving/minuteLeaving
-//move hourLeaving/minuteLeaving to global dictionary 
